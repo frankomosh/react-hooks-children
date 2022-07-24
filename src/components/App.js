9e0fe9cf43ee259c60dcf5b441ea3b3d5a454949
Frank-197 <frankomosh197@gmail.com>
2022-07-24 21:26:17 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "./Container";
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <Container header="Example header" direction="horizontal">
